25fac1fe8e6b4920d4bfbed0147e45b713db05d8
Phu Tang <phutanghong@gmail.com>
2018-09-10 14:19:21 +0700

    Update snackbar new material template

--- src/components/notification/index.js
+++ src/components/notification/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import Snackbar from 'material-ui/Snackbar';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import {
   shouldShowNotificationSelector,
